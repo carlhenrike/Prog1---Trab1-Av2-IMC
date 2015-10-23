6102f3ab571ea2d70cd9c0b6ec6858ab4534fa94
carlhenrike <carl.henrike91@gmail.com>
2015-10-22 23:17:44 -0200

    Cacule o IMC

---
+++ IMC.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	
+	float p,a;
+	float imc;
+
+	
+	printf("Digite Peso:\n");
+	scanf("%f", &p);
+	printf("Digite Altura:\n");
+	scanf("%f", &a);
+	
+	imc=p/(a*a);
+	
+	
+	if(imc<=19)
+		{
+		printf("Seu imc e: %.2f\nVoce esta muito magro\n",imc);
+	}
+	if(imc>=20&&imc<=25)
+
+	
+	{
+	   printf("Seu imc e: %.2f\nSeu esta Peso Ideal\n",imc);	
+	  	
+	}
+	
+	if(imc>=26&&imc<=30)
+	{
+		printf("Seu imc e: %.2f\n Voce esta Acima do peso\n",imc);
+	}
+	
+	if(imc>=31)
+	{
+ 
+		printf("Seu imc e: %.2f\nVoce esta obeso\n",imc);
+ }
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
